4a6e53edd80d268f811380b4750d378355845f16
Jefferson Cabrera Villacorta <jeffersoncabreradev@gmail.com>
2020-04-26 21:13:43 -0500

    Inicio Proyecto

---
+++ caseritaPeru/src/main/java/com/grupo04/caseritaPeru/CaseritaPeruApplication.java
@@ -0,0 +1,13 @@
+package com.grupo04.caseritaPeru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseritaPeruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseritaPeruApplication.class, args);
+	}
+
+}
---
+++ caseritaPeru/src/test/java/com/grupo04/caseritaPeru/CaseritaPeruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo04.caseritaPeru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseritaPeruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
